712e35e741b84f353dd3c7813c5ba3af034d650c
Osorabrian <osorabrian@gmail.com>
2026-04-14 19:04:37 -0500

    create a subdependency that injects to the dependency

--- dependency/main.py
+++ dependency/main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI, Depends, Body
+from fastapi import FastAPI, Depends, Body, Cookie
 from pydantic import BaseModel, EmailStr, Field
 from typing import Annotated
 
@@ -16,16 +16,33 @@ class CommonParams:
         self.user = user
 
 #we can create a functional dependency
-async def common_parameters(username: str, user: Annotated[User, Body()]):
-    return user
+#async def common_parameters(username: str, user: Annotated[User, Body()]):
+#    return user
 
 #declare the line of code as a variable to avoid code repetition
-common_dependencies = Annotated[dict, Depends(common_parameters)]
+#common_dependencies = Annotated[dict, Depends(common_parameters)]
 
+#This is a subdependency
+def query_extractor(q: str | None = None):
+    return q
+
+#This is  a dependency that depends on the subdependency
+def query_or_cookie_extractor(q: Annotated[str, Depends(query_extractor)], last_query: Annotated[str | None, Cookie()] = None):
+    if q:
+        return {"q": q}
+    else:
+        return {"last_query": last_query}
+
+#we use the dependency that calls a subdependency
 @app.get("/user/{username}")
-async def retrieve_user(params: common_dependencies):
-    return params
+async def retrieve_user(username: str, query_or_default: Annotated[str, Depends(query_or_cookie_extractor)]):
+    return username
+
+#utilizing the calss parameter
+@app.put("/user/{username}")
+async def put_user(params: Annotated[CommonParams, Depends()]):
+    return params.username
 
 @app.patch("/user/{username}")
-async def update_user(params: common_dependencies):
-    return params["username"]  
+async def update_user(params: Annotated[CommonParams, Depends()]):
+    return params.username
